a004bd6786a77ba074639e7cd12645055fe8f2a3
Napeace <najminafis1504@gmail.com>
2024-09-26 13:44:05 +0700

    test

--- Tugas Polymorphism/Program.cs
+++ Tugas Polymorphism/Program.cs
@@ -1,14 +1,23 @@
 ﻿using System;
+using System.Security.Cryptography.X509Certificates;
 
 class Hewan
 {
     public string Nama;
     public int Umur;
 
-    public Hewan(string nama , int umur)
+    public Hewan(string nama, int umur)
     {
         Nama = nama;
         Umur = umur;
+    }
+    public virtual string Suara()
+    {
+        return "Hewan ini bersuara";
+    }
+    public virtual string InfoHewan()
+    {
+        return $"Nama = {Nama}, Umur = {Umur} tahun";
     }
 }
 
@@ -20,6 +29,10 @@ class Mamalia : Hewan
     {
         jumlahKaki = JumlahKaki;
     }
+    public override string Suara()
+    {
+        return "Mamalia Bersuara";
+    }
 }
 
 class Reptil : Hewan
@@ -30,29 +43,112 @@ class Reptil : Hewan
     {
         panjangTubuh = PanjangTubuh;
     }
+    public override string Suara()
+    {
+        return "Reptil Bersuara";
+    }
 }
 
 class Singa : Mamalia
 {
     public Singa(string nama, int umur) : base(nama, umur, 4) { }
+    public override string Suara()
+    {
+        return "Rawr";
+    }
+    public void Mengaum()
+    {
+        Console.WriteLine("Singa Mengaum");
+    }
 }
 
 class Gajah : Mamalia
 {
     public Gajah(string nama, int umur) : base(nama, umur, 4) { }
+    public override string Suara()
+    {
+        return "Teeeeeeeeeeeeeeet";
+    }
 }
 
 class Ular : Reptil
 {
     public Ular(string nama, int umur, double panjangTubuh) : base(nama, umur, panjangTubuh) { }
+    public override string Suara()
+    {
+        return "Sssssssssss";
+    }
+    public void Merayap()
+    {
+        Console.WriteLine("Ular Merayap");
+    }
 }
 
 class Buaya : Reptil
 {
     public Buaya(string nama, int umur, double panjangTubuh) : base(nama, umur, panjangTubuh) { }
+    public override string Suara()
+    {
+        return "Buaya Bersuara";
+    }
 }
 
 class KebunBinatang
 {
-    
+    private Hewan[] koleksiHewan;
+    private int jumlahHewan;
+
+    public KebunBinatang(int kapasitas)
+    {
+        koleksiHewan = new Hewan[kapasitas];
+        jumlahHewan = 0;
+    }
+    public void TambahHewan(Hewan hewan)
+    {
+        if (jumlahHewan < koleksiHewan.Length)
+        {
+            koleksiHewan[jumlahHewan] = hewan;
+            jumlahHewan++;
+            Console.WriteLine($"{hewan.Nama} telah ditambahkan ke kebun binatang");
+        }
+        else
+        {
+            Console.WriteLine("Kebun Binatang penuh.");
+
+        }
+    }
+    public void DaftarHewan()
+    {
+        for (int i = 0; i < jumlahHewan; i++)
+        {
+            Console.WriteLine(koleksiHewan[i].InfoHewan());
+        }
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        KebunBinatang kebunBinatang = new KebunBinatang(4);
+        Singa singa = new Singa("Singa 1", 5);
+        Gajah gajah = new Gajah("Gajah 1", 5);
+        Ular ular = new Ular("Ular 1", 5, 4);
+        Buaya buaya = new Buaya("Buaya 1", 5, 4);
+
+        kebunBinatang.TambahHewan(singa);
+        kebunBinatang.TambahHewan(gajah);
+        kebunBinatang.TambahHewan(ular);
+        kebunBinatang.TambahHewan(buaya);
+
+        kebunBinatang.DaftarHewan();
+
+        Console.WriteLine(singa.Suara());
+        Console.WriteLine(gajah.Suara());
+        Console.WriteLine(ular.Suara());
+        Console.WriteLine(buaya.Suara());
+
+        Console.WriteLine("\nMethod khusus Singa:");
+        singa.Mengaum();
+    }
 }
--- Tugas Polymorphism/obj/Release/net8.0/Tugas Polymorphism.AssemblyInfo.cs
+++ Tugas Polymorphism/obj/Release/net8.0/Tugas Polymorphism.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("Tugas Polymorphism")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0cbe0ae6bdfd3e8a8dcd70f31b84258604692267")]
 [assembly: System.Reflection.AssemblyProductAttribute("Tugas Polymorphism")]
